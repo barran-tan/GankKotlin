83f72d9a9e6d862b337716ebca02954441ece342
tanwei <tanwei@changingedu.com>
2017-09-26 16:46:27 +0800

    start project

---
+++ app/src/main/java/com/barran/gank/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.barran.gank.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.barran.gank.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/barran/gank/service/APIService.java
@@ -0,0 +1,11 @@
+package com.barran.gank.service;
+
+/**
+ * gank.io/api
+ *
+ * Created by tanwei on 2017/9/26.
+ */
+
+public interface APIService {
+
+}
